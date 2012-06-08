fb20a9358887f3fbef283fc989fedb40acda6eac
Brandon Alexander <baalexander@gmail.com>
2012-06-07 21:58:58 -0500

    All example is ready to run.

--- example/example.js
+++ example/example.js
@@ -1,87 +1,91 @@
 var ros = new ROS('ws://localhost:9090');
 
-// // Topics
-// // ------
+ros.on('error', function(error) {
+  console.log(error);
+});
 
-// ros.types([
-//   'std_msgs/String'
-// , 'geometry_msgs/Twist'
-// ], function(String, Twist) {
-//   console.log('Created message types');
+// Topics
+// ------
 
-//   // Create a new topic, /cmd_vel
-//   var cmdVel = new ros.topic({
-//     node        : 'talker'
-//   , topic       : 'cmd_vel'
-//   , messageType : Twist
-//   });
-//   console.log('Created topic ' + cmdVel.topic);
+ros.types([
+  'std_msgs/String'
+, 'geometry_msgs/Twist'
+], function(String, Twist) {
+  console.log('Created message types');
 
-//   // Listen for errors on /cmd_vel
-//   cmdVel.on('error', function(error) {
-//     console.log('cmdVel error: ' + error);
-//   });
+  // Create a new topic, /cmd_vel
+  var cmdVel = new ros.topic({
+    node        : 'talker'
+  , topic       : 'cmd_vel'
+  , messageType : Twist
+  });
+  console.log('Created topic ' + cmdVel.topic);
 
-//   var twist = new Twist({
-//     angular: {
-//       x: 1
-//     , y: 0
-//     , z: 0
-//     }
-//   , linear: {
-//       x: 0
-//     , y: 0
-//     , z: 0
-//     }
-//   });
-//   cmdVel.publish(twist);
-//   console.log('Published message on ' + cmdVel.topic);
+  // Listen for errors on /cmd_vel
+  cmdVel.on('error', function(error) {
+    console.log('cmdVel error: ' + error);
+  });
 
-//   // Subscribe to messages on the /cmd_vel topic
-//   var listener = new ros.topic({
-//     node        : 'talker'
-//   , topic       : '/listener'
-//   , messageType : String
-//   });
-//   console.log('Created topic ' + listener.topic);
-//   listener.subscribe(function(message) {
-//     console.log('Received message on ' + listener.topic + ': ' + message.data);
-//   });
+  var twist = new Twist({
+    angular: {
+      x: 1
+    , y: 0
+    , z: 0
+    }
+  , linear: {
+      x: 0
+    , y: 0
+    , z: 0
+    }
+  });
+  cmdVel.publish(twist);
+  console.log('Published message on ' + cmdVel.topic);
 
-//   ros.getTopics(function(topics) {
-//     console.log('Current topics in ROS:' + topics);
-//   });
+  // Subscribe to messages on the /cmd_vel topic
+  var listener = new ros.topic({
+    node        : 'talker'
+  , topic       : '/listener'
+  , messageType : String
+  });
+  console.log('Created topic ' + listener.topic);
+  listener.subscribe(function(message) {
+    console.log('Received message on ' + listener.topic + ': ' + message.data);
+  });
 
-// });
+  ros.getTopics(function(topics) {
+    console.log('Current topics in ROS:' + topics);
+  });
+
+});
 
 
 // Services
 // --------
 
-// ros.services([
-//   'rospy_tutorials/AddTwoInts'
-// ], function(AddTwoInts) {
-//   // Create a new service client
-//   var addTwoIntsClient = new ros.service({
-//     name        : '/add_two_ints'
-//   , serviceType : AddTwoInts
-//   });
+ros.services([
+  'rospy_tutorials/AddTwoInts'
+], function(AddTwoInts) {
+  // Create a new service client
+  var addTwoIntsClient = new ros.service({
+    name        : '/add_two_ints'
+  , serviceType : AddTwoInts
+  });
 
-//   // Listen for errors when calling /add_two_ints
-//   addTwoIntsClient.on('error', function(error) {
-//     console.log('add_two_ints error: ' + error);
-//   });
+  // Listen for errors when calling /add_two_ints
+  addTwoIntsClient.on('error', function(error) {
+    console.log('add_two_ints error: ' + error);
+  });
 
-//   // Call the service with a callback for the results
-//   var request = new AddTwoInts.request({ A: 1, B: 2});
-//   addTwoIntsClient.callService(request, function(result) {
-//     console.log('Result for service call on ' + addTwoIntsClient.name + ': ' + result.sum);
-//   });
+  // Call the service with a callback for the results
+  var request = new AddTwoInts.request({ A: 1, B: 2});
+  addTwoIntsClient.callService(request, function(result) {
+    console.log('Result for service call on ' + addTwoIntsClient.name + ': ' + result.sum);
+  });
 
-//   ros.getServices(function(services) {
-//     console.log('Current services in ROS:' + services);
-//   });
-// });
+  ros.getServices(function(services) {
+    console.log('Current services in ROS:' + services);
+  });
+});
 
 
 // Params
