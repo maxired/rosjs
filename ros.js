91ab243efa13c834ff4a995135fbc0f31de12136
Brandon Alexander <baalexander@gmail.com>
2012-07-01 22:07:29 -0500

    Adds comments/documentation to the ros.js file.

--- ros.js
+++ ros.js
@@ -1,3 +1,10 @@
+// Ros.js can be included using <script src="ros.js"> or AMD.  The next few
+// lines provide support for both formats and are based on the Universal Module
+// Definition.
+//
+// See:
+//  * AMD - http://bryanforbes.github.com/amd-commonjs-modules-presentation/2011-10-29/)
+//  * UMD - https://github.com/umdjs/umd/blob/master/amdWeb.js
 (function (root, factory) {
   if (typeof define === 'function' && define.amd) {
     define(['eventemitter2'], factory);
@@ -7,12 +14,16 @@
   }
 }(this, function (EventEmitter2) {
 
+  // Takes in the URL of the WebSocket server.
+  // Emits the following events:
+  //  * 'error' - there was an error with ROS
+  //  * 'connection' - connected to the WebSocket server
+  //  * 'close' - disconnected to the WebSocket server
   var ROS = function(url) {
     var ros = this;
 
-
-    // Socket Handler
-    // --------------
+    // Socket Handling
+    // ---------------
 
     var socket = new WebSocket(url);
     socket.onopen = function(event) {
@@ -24,6 +35,8 @@
     socket.onerror = function(event) {
       ros.emit('error', event);
     };
+    // Parses message responses from rosbridge and sends to the appropriate
+    // topic, service, or param.
     socket.onmessage = function(message) {
       var data = JSON.parse(message.data);
       if (data.receiver) {
@@ -31,6 +44,8 @@
       }
     };
 
+    // Sends the message over the WebSocket, but queues the message up if not
+    // yet connected.
     function callOnConnection(message) {
       var messageJson = JSON.stringify(message);
       if (socket.readyState !== WebSocket.OPEN) {
@@ -43,10 +58,10 @@
       }
     };
 
-
     // Topics
     // ------
 
+    // Retrieves list of topics in ROS as an array.
     ros.getTopicList = function(callback) {
       ros.once('/rosjs/topics', function(data) {
         callback(data);
@@ -58,6 +73,9 @@
       callOnConnection(call);
     };
 
+    // Message objects are used for publishing and subscribing to and from
+    // topics. Takes in an object matching the fields defined in the .msg
+    // definition file.
     ros.Message = function(values) {
       var message = this;
       if (values) {
@@ -67,6 +85,10 @@
       }
     }
 
+    // Publish and/or subscribe to a topic in ROS. Options include:
+    //  * node - the name of the node to register under
+    //  * name - the topic name, like /cmd_vel
+    //  * messageType - the message type, like 'std_msgs/String'
     ros.Topic = function(options) {
       var topic         = this;
       options           = options || {};
@@ -74,6 +96,8 @@
       topic.name        = options.name;
       topic.messageType = options.messageType
 
+      // Every time a message is published for the given topic, the callback
+      // will be called with the message object.
       topic.subscribe = function(callback) {
         topic.on('message', function(message) {
           callback(message);
@@ -90,6 +114,8 @@
         callOnConnection(call);
       };
 
+      // Unregisters as a subscriber for the topic. Unsubscribing will remove
+      // all subscribe callbacks.
       topic.unregisterSubscriber = function() {
         ros.removeAllListeners([topic.name]);
         var call = {
@@ -99,6 +125,7 @@
         callOnConnection(call);
       };
 
+      // Publish the message. Takes in a ros.Message.
       topic.publish = function(message) {
         var call = {
           receiver : topic.name
@@ -110,10 +137,10 @@
     };
     ros.Topic.prototype.__proto__ = EventEmitter2.prototype;
 
-
     // Services
     // --------
 
+    // Retrieves list of active service names in ROS as an array.
     ros.getServiceList = function(callback) {
       ros.once('/rosjs/services', function(data) {
         callback(data);
@@ -125,6 +152,8 @@
       callOnConnection(call);
     };
 
+    // A ServiceRequest is passed into the service call. Takes in an object
+    // matching the values of the request part from the .srv file.
     ros.ServiceRequest = function(values) {
       var serviceRequest = this;
       if (values) {
@@ -134,6 +163,8 @@
       }
     }
 
+    // A ServiceResponse is returned from the service call. Takes in an object
+    // matching the values of the response part from the .srv file.
     ros.ServiceResponse = function(values) {
       var serviceResponse = this;
       if (values) {
@@ -143,12 +174,16 @@
       }
     }
 
+    // A ROS service client. Options include:
+    //  * name - the service name, like /add_two_ints
+    //  * serviceType - the service type, like 'rospy_tutorials/AddTwoInts'
     ros.Service = function(options) {
       var service         = this;
       options             = options || {};
       service.name        = options.name;
       service.serviceType = options.serviceType;
 
+      // Calls the service. Returns the service response in the callback.
       service.callService = function(request, callback) {
         ros.once(service.name, function(data) {
           var response = new ros.ServiceResponse(data);
@@ -167,10 +202,10 @@
     };
     ros.Service.prototype.__proto__ = EventEmitter2.prototype;
 
-
     // Params
     // ------
 
+    // Retrieves list of param names from the ROS Parameter Server as an array.
     ros.getParamList = function(callback) {
       ros.once('/rosjs/get_param_names', function(data) {
         callback(data);
@@ -182,11 +217,14 @@
       callOnConnection(call);
     };
 
+    // A ROS param. Options include:
+    //  * name - the param name, like max_vel_x
     ros.Param = function(options) {
       var param  = this;
       options    = options || {};
       param.name = options.name;
 
+      // Fetches the value of the param and returns in the callback.
       param.get = function(callback) {
         ros.once('/rosjs/get_param', function(value) {
           callback(value);
@@ -198,6 +236,7 @@
         callOnConnection(call);
       };
 
+      // Sets the value of the param in ROS.
       param.set = function(value) {
         var call = {
           receiver : '/rosjs/set_param'
